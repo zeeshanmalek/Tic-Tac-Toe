c8d90faf7f6e716793fdc01eb15dfa8898b505fc
zeeshanmalek <jisanahmadmalek@.com>
2023-10-25 14:32:41 +0530

    installed scsss

---
+++ src/Components/Square.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const Square = (props) => {
+  return (
+    <div>
+      <h1>{props.value}</h1>
+    </div>
+  )
+}
+
+export default Square
